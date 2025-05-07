7cee04a3eed9928ca6c0281d5721865066722dc7
dohieunghia2002 <jeetkunedo10000@gmail.com>
2025-05-07 13:42:40 +0700

    middlewares base

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import userController from '../controllers/user.controller.js';
+import middlewares from '../middlewares/token.js';
 
 const router = express.Router();
 
-router.get('/all', userController.getAllUser);
+router.get('/all', middlewares.verifyTokenAdmin, userController.getAllUser);
 
 export default router
